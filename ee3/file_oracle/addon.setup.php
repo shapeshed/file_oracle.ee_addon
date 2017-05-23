c6889252cff8595bc9a61a7b7df848d4473aa288
noellekimiko <noelle.fakaji@yahoo.com>
2017-05-23 12:44:11 -0700

    Updated for EE3: Added addon.setup.php file, removed $plugin_info
    Following the ExpressionEngine conversion guides for moving from EE2 to EE3, the required addon.setup.php file has been added using the information from the old $plugin_info array. No other changes have been made to the source.
    
    Information about the plugin changes in eE3 (there's not many):
    https://docs.expressionengine.com/latest/development/conversion/plugins.html

---
+++ ee3/file_oracle/addon.setup.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'author'      => 'George Ornbo',
+  'author_url'  => 'http://shapeshed.com/',
+  'name'        => 'File Oracle',
+  'description' => 'Provides information on a file',
+  'version'     => '2.0.1',
+  'namespace'   => 'File_oracle'
+);
